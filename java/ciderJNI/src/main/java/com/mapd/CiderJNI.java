df30f09ce6c5a20c822cc935ded9b0f9603a5269
jikunshang <kunshang.ji@intel.com>
2021-07-27 17:44:02 +0800

    refactor cider jni interface, will init DBengine when load library. DBengine will be singleton in one process. (#28)

--- java/ciderJNI/src/main/java/com/mapd/CiderJNI.java
+++ java/ciderJNI/src/main/java/com/mapd/CiderJNI.java
@@ -4,16 +4,23 @@ import java.io.IOException;
 
 public class CiderJNI {
 
-  static {
-    System.loadLibrary("ciderjni");
-  }
+    private static long ptr = 0;
 
-  public static native int processBlocks(long dbePtr, String sql, String schema,
-                                         long[] dataBuffers, long[] dataNulls,
-                                         long[] resultBuffers, long[] resultNulls,
-                                         int rowCount);
+    static {
+        System.loadLibrary("ciderjni");
+        if (ptr == 0) ptr = init();
+    }
 
-  public static native long init();
+    public static native int processBlocks(long dbePtr, String sql, String schema,
+                                           long[] dataBuffers, long[] dataNulls,
+                                           long[] resultBuffers, long[] resultNulls,
+                                           int rowCount);
 
-  public static native void close(long dbePtr);
+    public static native long init();
+
+    public static native void close(long dbePtr);
+
+    public static long getPtr() {
+        return ptr;
+    }
 }
